42ac5abf9a7d94ea32f632bb8d917b3b2047ed3f
MarioDelToro-UPM <mario.deltoro.armas@alumnos.upm.es>
2024-12-26 19:42:53 +0000

    Added finalized ignition map

---
+++ plot_3d.m
@@ -0,0 +1,16 @@
+load("ignitionmap.mat")
+Y = zeros(33,18); Z = zeros(33,18); 
+for i = 1:33
+Y(i,:)=y;
+end 
+for i = 1:18
+Z(:,i)=z;
+end 
+figure
+surf(Y,Z,x)
+axis([0.4 2.1 1000 9000 -20 50])
+xlabel('Presión de admisión [Pa]')
+ylabel('[rpm]')
+zlabel('AICB [º]')
+filename='ignitionmap.jpg';
+print(gcf, filename, '-djpeg', '-r300');
